645383d659437743429e40513937e1e1e6cc4416
William Haug <WH@Skytale.no>
2024-06-12 12:28:02 +0200

    Added icons to side bar buttons

--- my-app/src/pages/shared/Sidebar.tsx
+++ my-app/src/pages/shared/Sidebar.tsx
@@ -1,33 +1,31 @@
 // src/pages/shared/Sidebar.tsx
 import React from 'react';
-import { Box, VStack, Text } from '@chakra-ui/react';
+import { Box, VStack, Text, Image } from '@chakra-ui/react';
 import { NavLink } from 'react-router-dom';
+import groupIcon from '@/assets/scss/images/Group.svg';
+import userIcon from '@/assets/scss/images/user.svg';
+import slidersIcon from '@/assets/scss/images/Sliders.svg';
+import licenseIcon from '@/assets/scss/images/Group57.svg';
+import './Sidebar.css';
 
 const Sidebar: React.FC = () => {
-  const linkStyle = {
-    display: 'flex',
-    width: '100%', // Ensure the link takes the full width of the sidebar
-    padding: '9px 0px 10px 20px',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    flexShrink: 0,
-    borderBottom: '3px solid var(--Ocean-2, #1E2455)',
-    background: 'var(--Ocean-3, #212D63)',
-  };
-
   return (
-    <Box width="250px" backgroundColor="var(--Ocean-3, #212D63)" color="white" height="100vh">
+    <Box className="sidebar-container">
       <VStack align="start" spacing={0}>
-        <NavLink to="/groups" style={{ ...linkStyle, borderTop: '3px solid var(--Ocean-2, #1E2455)' }}>
+        <NavLink to="/groups" className="sidebar-link sidebar-link-top">
+          <Image src={groupIcon} alt="Group Icon" className="sidebar-icon" />
           <Text>Grupper</Text>
         </NavLink>
-        <NavLink to="/users" style={linkStyle}>
+        <NavLink to="/users" className="sidebar-link">
+          <Image src={userIcon} alt="User Icon" className="sidebar-icon" />
           <Text>Brukere</Text>
         </NavLink>
-        <NavLink to="/configurations" style={linkStyle}>
+        <NavLink to="/configurations" className="sidebar-link">
+          <Image src={slidersIcon} alt="Sliders Icon" className="sidebar-icon" />
           <Text>Konfigurasjoner</Text>
         </NavLink>
-        <NavLink to="/licenses" style={linkStyle}>
+        <NavLink to="/licenses" className="sidebar-link">
+          <Image src={licenseIcon} alt="License Icon" className="sidebar-icon" />
           <Text>Lisens</Text>
         </NavLink>
       </VStack>
